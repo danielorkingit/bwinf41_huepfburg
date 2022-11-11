e3c3ca285154476a3fd95623ef8cfae6d40a18f1
danielorkingit <danielorkin@icloud.com>
2022-11-11 17:28:41 +0100

    update

--- src/bwinf41_huepfburg/Main.java
+++ src/bwinf41_huepfburg/Main.java
@@ -1,6 +1,8 @@
 package bwinf41_huepfburg;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -69,7 +71,7 @@ public class Main {
 			System.out.println("Map doesn't contain a entry point."); 
 		} else {
 			
-			// Map mit der ersten Ebene an Werten vorbereitem
+			// Map mit der ersten Ebene an Werten vorbereiten
 			
 			ArrayList<Integer> index1 = getIndex(startPoint, 1);
 			ArrayList<Integer> index2 = getIndex(startPoint, 2);
@@ -101,85 +103,92 @@ public class Main {
 	}
 		
 
-	private static boolean calculate(Map<Integer, Map<Integer, Integer>> tree1, Map<Integer, Map<Integer, Integer>> tree2, ArrayList<Integer> startPoint, ArrayList<Integer> endPoint, int index) {
-
-			Map<Integer, Integer> level1 = tree1.get(index-1);
-			Map<Integer, Integer> level2 = tree2.get(index-1);
-			
-			Map<Integer, Integer> return1 = new HashMap<Integer, Integer>();
-			Map<Integer, Integer> return2 = new HashMap<Integer, Integer>();
-
-			for (var i : level1.entrySet()) {
-				ArrayList<Integer> tmp = getIndex(startPoint, i.getKey());
-				tmp = getEquivalent(endPoint, tmp);
+	private static void calculate(Map<Integer, Map<Integer, Integer>> tree1, Map<Integer, Map<Integer, Integer>> tree2, ArrayList<Integer> startPoint, ArrayList<Integer> endPoint, int index) {
+			try {
+				Map<Integer, Integer> level1 = tree1.get(index-1);
+				Map<Integer, Integer> level2 = tree2.get(index-1);
 				
-				for (int h : tmp) {
-					return1.put(h, i.getKey());
-				}
-			}
-			
-			for (var i : level2.entrySet()) {
-				ArrayList<Integer> tmp = getIndex(startPoint, i.getKey());
-				tmp = getEquivalent(endPoint, tmp);
-				
-				for (int h : tmp) {
-					return2.put(h, i.getKey());
-				}
-			}
-			
-			// Neue "Kinder" an kompletten Baum anbinden
-			
-			if (!return1.isEmpty()) {
-				tree1.put(index, return1);
-			} else {
-				return false;
-			}
-			
-			if (!return2.isEmpty()) {
-				tree2.put(index, return2);
-			} else {
-				return false;
-			}
-			
-			index = index +1;
-			
-			// Zwei Listen auf einen gleichen Wert überprüfen
-			
-			ArrayList<Integer> l1 = new ArrayList<>(return1.keySet());
-			ArrayList<Integer> l2 = new ArrayList<>(return2.keySet());
-			
-			l1.retainAll(l2);
-			
-			if(l1.isEmpty()) {
-				calculate(tree1, tree2, startPoint, endPoint, index);
-			} else {
-				System.out.println("Erster Pfad (Sasha):");
-				System.out.println(l1.get(0));
-				calculatePath(tree1, index, l1.get(0));
-				
-				System.out.println("\nZweiter Pfad (Mika):");
-				System.out.println(l1.get(0));
-				calculatePath(tree2, index, l1.get(0));
-				
-				System.out.printf("\nBeide landen auf gleichzeitig nach %d Sprüngen auf Feld %d.", index, l1.get(0));
-			}
+				Map<Integer, Integer> return1 = new HashMap<Integer, Integer>();
+				Map<Integer, Integer> return2 = new HashMap<Integer, Integer>();
+	
+				for (var i : level1.entrySet()) {
+					ArrayList<Integer> tmp = getIndex(startPoint, i.getKey());
+					tmp = getEquivalent(endPoint, tmp);
 					
-			return false;
+					for (int h : tmp) {
+						return1.put(h, i.getKey());
+					}
+				}
+				
+				for (var i : level2.entrySet()) {
+					ArrayList<Integer> tmp = getIndex(startPoint, i.getKey());
+					tmp = getEquivalent(endPoint, tmp);
+					
+					for (int h : tmp) {
+						return2.put(h, i.getKey());
+					}
+				}
+				
+				// Neue "Kinder" an kompletten Baum anbinden
+				
+				if (!return1.isEmpty()) {
+					tree1.put(index, return1);
+				} 
+				
+				if (!return2.isEmpty()) {
+					tree2.put(index, return2);
+				} 
+				
+				index = index +1;
+				
+				// Zwei Listen auf einen gleichen Wert überprüfen
+				
+				ArrayList<Integer> l1 = new ArrayList<>(return1.keySet());
+				ArrayList<Integer> l2 = new ArrayList<>(return2.keySet());
+				
+				l1.retainAll(l2);
+				
+				if(l1.isEmpty()) {
+					calculate(tree1, tree2, startPoint, endPoint, index);
+				} else {
+					System.out.println("Erster Pfad (Sasha):");
+					ArrayList<Integer> path1 = calculatePath(tree1, index, l1.get(0), new ArrayList<Integer>());
+					Collections.reverse(path1);
+					path1.forEach((Integer i) -> {
+						System.out.println(i);
+					});
+					System.out.println(l1.get(0));
+					
+					System.out.println("\nZweiter Pfad (Mika):");
+					ArrayList<Integer> path2 = calculatePath(tree2, index, l1.get(0), new ArrayList<Integer>());
+					Collections.reverse(path2);
+					path2.forEach((Integer i) -> {
+						System.out.println(i);
+					});
+					System.out.println(l1.get(0));
+					
+					System.out.printf("\nBeide landen auf gleichzeitig nach %d Sprüngen auf Feld %d.", index, l1.get(0));
+				}
+						
+			} catch (StackOverflowError e) {
+				System.out.println("Mika und Sasha treffen sich niemals.");
+			}
 	}
 
-	private static void calculatePath(Map<Integer, Map<Integer, Integer>> tree, int index, int child) {
+	private static ArrayList<Integer> calculatePath(Map<Integer, Map<Integer, Integer>> tree, int index, int child, ArrayList<Integer> process) {
 		
 		Map<Integer, Integer> level1 = tree.get(index-1);
 							
 		child = level1.get(child);
 			
-		System.out.println(child);
+		process.add(child);
 			
 		if(child == 1 || child == 2) {
-			return;
+			return process;
 		} else {
-			calculatePath(tree, index-1, child);
+			calculatePath(tree, index-1, child, process);
 		}
+		return process;
 		
 	}
 
@@ -197,13 +206,15 @@ public class Main {
 
 	private static ArrayList<Integer> getIndex(ArrayList<Integer> array, int i) {
 		
+		ArrayList<Integer> copy = new ArrayList<>(array);
 		ArrayList<Integer> result = new ArrayList<>();
 		
-		while (array.indexOf(i) != -1) {
-			int x = array.indexOf(i);
+		while (copy.indexOf(i) != -1) {
+			int x = copy.indexOf(i);
 			result.add(x);
-			array.set(x, -1);
+			copy.set(x, -1);
 		}
+		
 		
 		return result;
 	}
